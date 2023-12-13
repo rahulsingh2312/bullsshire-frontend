5634b4e6b8a0696f437a35dec6396a68dfd5e3f4
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2023-12-13 14:04:41 +0530

    landin page ends here

--- src/app/hero/footer.jsx
+++ src/app/hero/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Logo from './LOGO.png';
 import './Footer.css';
-import { oxygen } from '../font'
+import { playfairDisplaySC , oxygen } from '../font'
 import {   IoLocationSharp , IoCallOutline , IoMailOutline  , } from "react-icons/io5";
 import { FaRedditSquare , FaTelegram,FaTwitter , FaInstagram} from "react-icons/fa"
 
@@ -10,13 +10,13 @@ export default function Footer () {
   return (
 <>
 <div>
-  <div className={`flex justify-center ${oxygen.className} md:text-2xl text-sm`}>
+  <div className={`flex justify-center ${oxygen.className} md:text-2xl text-xs`}>
 <Image className='w-40 md:w-1/5' src={Logo} alt="inlogo" />
 <div className='md:ml-40 ml-5 mt-7 md:mt-20'>
   <div className="flex">
-   <IoLocationSharp /> Andheri , India
+   <IoLocationSharp className='mr-2 mt-1' /> Andheri , India
   </div>
-  <div className='md:flex md:my-10 my-4   '> <div className="flex"> <IoCallOutline /> +91 9321441951</div> <div className="flex md:ml-10"><IoMailOutline /> bullsshiretraders@gmail.com </div></div>
+  <div className='md:flex md:my-10 my-4   '> <div className="flex"> <IoCallOutline className='mr-2 mt-1' /> +91 9321441951</div> <div className="flex md:ml-10"><IoMailOutline className='mr-2 mt-1'  /> bullsshiretraders@gmail.com </div></div>
   <div className="flex">
 	  social :3
    
@@ -24,7 +24,7 @@ export default function Footer () {
 	  
 	
 	  </div>
-	  <div className="flex  md:mt-5 mt-3 mb-3 md:mb-5">
+	  <div className='flex  md:mt-5 mt-3 mb-3 md:mb-5'>
 	 < FaInstagram className="md:mr-5 mr-2" />
 	  <FaTelegram className="md:mr-5 mr-2"/>
 	  <FaTwitter className="md:mr-5 mr-2" />
@@ -35,9 +35,10 @@ export default function Footer () {
 <div className='border mt-5'>
 
 </div>
+
 <div className='flex text-sm  my-2'>
-<div className="mr-5 ml-2 md:mx-20"> home </div>
-	  <div className="mr-5 md:mr-20" >help  </div> <div> support</div>
+<div className={` ${playfairDisplaySC.className} mr-5 md:mx-20 ml-2`}> home </div>
+	  <div className={` ${playfairDisplaySC.className} mr-5 md:mr-20`} >help  </div> <div className={` ${playfairDisplaySC.className} `}> support</div>
 <div className="ml-auto mr-5"> copyright © Bullsshire 2023 </div>
 </div>
 </div>
