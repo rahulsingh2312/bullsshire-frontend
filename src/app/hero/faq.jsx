98ad08cea0b7a078813bbf40b42a676727fa171b
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2023-12-13 18:32:43 +0530

    faq fix

--- src/app/hero/faq.jsx
+++ src/app/hero/faq.jsx
@@ -53,9 +53,9 @@ export default function Saq() {
     return (
         <>
         <div className="faq-container md:flex mt-20 "> {/* Apply additional styling using a CSS class */}
-         <Image className="w-1/2" src={Iagfaq} alt="faq"/>
-           <div className="md:mt-40 mt-20 md:text-4xl ">
-            <Faq className='w-1/2'
+         <Image className="md:w-1/2" src={Iagfaq} alt="faq"/>
+           <div className="md:mt-40 md:w-1/2 mt-20 md:text-4xl ">
+            <Faq 
                 data={data}
                 styles={styles}
                 config={config}
