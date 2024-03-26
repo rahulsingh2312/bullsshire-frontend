3b2501ca64802da8fcc05fb0bfc82c6e7d0586c6
Aman Prajapati <pjaman05671@gmail.com>
2024-03-26 22:17:20 +0530

    changed css

--- src/app/hero/whyin.jsx
+++ src/app/hero/whyin.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Whyin = () => {
   return (
     <>
-    <div className="flex justify-center items-center h-screen text-white">
+    <div className="flex justify-center items-center h-screen text-white mx-8">
         <div>
-      <h1 className="text-8xl font-black text-[#692469] mb-8">Why Inverock Wealth</h1>
-      <ul className='flex flex-col m-auto text-2xl'>
+      <h1 className="text-7xl md:text-8xl font-black text-[#692469] mb-8">Why Inverock Wealth</h1>
+      <ul className='flex flex-col m-auto text-2xl mx-8 list-disc'>
         <li>We are SEBI registered researched analyst</li>
         <li>Quant based research analyst</li>
         <li>Managed your Portfolio with our buy on dips strategy</li>
