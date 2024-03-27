6b4958ef0cab366f828a2b1cbfb0439ab71e8b77
Rahul <rahulsinghhh2312@gmail.com>
2024-03-27 10:02:31 +0530

    Update faq.jsx

--- src/app/hero/faq.jsx
+++ src/app/hero/faq.jsx
@@ -10,27 +10,21 @@ const data = {
     title: "Frequently Asked Questions",
     rows: [
         {
-            title: "How does it work?",
-            content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat,
-              ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus.
-              In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae.
-              Fusce sed commodo purus, at tempus turpis.`,
+            title: "What does Inverock Wealth do?",
+            content: `Inverock Wealth is a SEBI registered Research Analyst that provides data driven stock picks to investors and traders for Intraday trades as well as for long term wealth creation.`,
         },
         {
-            title: "What is Lorem Ipsum?",
+            title: "What is the minimum amount required for investment?",
             content:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
+                "You can start your investment with any amount of your choice. ",
         },
         {
-            title: "Lorem Ipsum dolor sit amet",
-            content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
-            Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
-            Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
-            Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
+            title: "How much fees are charged for the Research reports?",
+            content: `Currently, we are not charging anything for the research that we provide, it's FREE. `,
         },
         {
-            title: "What is the package version",
-            content: <p>The current version is 1.2.1</p>,
+            title: "Does Inverock Wealth have access to my capital?",
+            content: <p>Absolutely not. We don't have access to your capital or funds as it remains with you in your Demat account.</p>,
         },
     ],
 };
